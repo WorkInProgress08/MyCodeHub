49ec52ba5384578b30d943b9d6227dac67f12ff7
MissionRover002 <74752332+MissionRover002@users.noreply.github.com>
2022-08-16 02:50:27 +0530

    Simple codes
    I did this when I was learning java,

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Me.sayName(args[0]);
+        Me.sayAge(args[1]);
+    }
+}
---
+++ Me.java
@@ -0,0 +1,9 @@
+public class Me {
+    public static void sayName(String a) {
+        System.out.println(a);
+    }
+    public static void sayAge(String b) {
+        System.out.println(b);
+    }
+    
+}
